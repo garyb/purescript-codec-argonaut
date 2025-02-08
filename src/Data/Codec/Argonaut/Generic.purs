c7f490a2511e27493ca758ad87a392f2c861e0c3
Michael Bock <no-day@posteo.net>
2025-02-08 06:13:54 +0100

    Add nullarySumWith

--- src/Data/Codec/Argonaut/Generic.purs
+++ src/Data/Codec/Argonaut/Generic.purs
@@ -23,27 +23,57 @@ import Type.Proxy (Proxy(..))
 -- | decode (nullarySum "MySum") (J.fromString "MoarCtors") == Right MoarCtors
 -- |```
 nullarySum ∷ ∀ a r. Generic a r ⇒ NullarySumCodec r ⇒ String → CA.JsonCodec a
-nullarySum name =
+nullarySum name = nullarySumWith defaultNullarySumEncoding name
+
+type NullarySumEncoding =
+  { mapTag ∷ String → String
+  }
+
+defaultNullarySumEncoding ∷ NullarySumEncoding
+defaultNullarySumEncoding =
+  { mapTag: identity
+  }
+
+-- | Like nullarySum, but allows customizing the encoding with options.
+-- |
+-- | ```purescript
+-- | import Data.Argonaut as J
+-- |
+-- | data MySum = Ctor1 | Ctor2 | MoarCtors
+-- | derive instance genericMySum ∷ Generic MySum _
+-- |
+-- | let opts = { mapTag: \tag → "My" <> tag }
+-- |
+-- | encode (nullarySumWith opts "MySum") Ctor1 == J.fromString "MyCtor1"
+-- | decode (nullarySumWith opts "MySum") (J.fromString "MyMoarCtors") == Right MoarCtors
+-- |```
+nullarySumWith ∷ ∀ a r. Generic a r ⇒ NullarySumCodec r ⇒ NullarySumEncoding → String → CA.JsonCodec a
+nullarySumWith encoding name =
   C.codec'
-    (map to <<< nullarySumDecode name)
-    (nullarySumEncode <<< from)
+    (map to <<< nullarySumDecode encoding name)
+    (nullarySumEncode encoding <<< from)
 
 class NullarySumCodec r where
-  nullarySumEncode ∷ r → J.Json
-  nullarySumDecode ∷ String → J.Json → Either CA.JsonDecodeError r
+  nullarySumEncode ∷ NullarySumEncoding → r → J.Json
+  nullarySumDecode ∷ NullarySumEncoding → String → J.Json → Either CA.JsonDecodeError r
 
 instance nullarySumCodecSum ∷ (NullarySumCodec a, NullarySumCodec b) ⇒ NullarySumCodec (Sum a b) where
-  nullarySumEncode = case _ of
-    Inl a → nullarySumEncode a
-    Inr b → nullarySumEncode b
-  nullarySumDecode name j = Inl <$> nullarySumDecode name j
-    <|> Inr <$> nullarySumDecode name j
+  nullarySumEncode encoding = case _ of
+    Inl a → nullarySumEncode encoding a
+    Inr b → nullarySumEncode encoding b
+  nullarySumDecode encoding name j = Inl <$> nullarySumDecode encoding name j
+    <|> Inr <$> nullarySumDecode encoding name j
 
 instance nullarySumCodecCtor ∷ IsSymbol name ⇒ NullarySumCodec (Constructor name NoArguments) where
-  nullarySumEncode _ =
-    J.fromString $ reflectSymbol (Proxy ∷ Proxy name)
-  nullarySumDecode name j = do
-    tag ← note (CA.Named name (CA.TypeMismatch "String")) (J.toString j)
+  nullarySumEncode encoding _ =
+    let
+      tagRaw = reflectSymbol (Proxy ∷ Proxy name)
+      tag = encoding.mapTag tagRaw
+    in
+      J.fromString $ tag
+  nullarySumDecode encoding name j = do
+    tagRaw ← note (CA.Named name (CA.TypeMismatch "String")) (J.toString j)
+    let tag = encoding.mapTag tagRaw
     if tag /= reflectSymbol (Proxy ∷ Proxy name) then
       Left (CA.Named name (CA.UnexpectedValue j))
     else
